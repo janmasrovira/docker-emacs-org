ee692747521c5892c38f15756e9d38fc6cdb1c47
Jan Mas Rovira <janmasrovira@gmail.com>
2020-07-10 15:25:27 +0200

    add koma-script support for report and article

--- init.el
+++ init.el
@@ -1,4 +1,23 @@
 (require 'org-ref)
 (setq org-latex-default-packages-alist nil)
+(setq org-latex-caption-above nil)
 (princ (format "org-version: %s\n" (org-version)))
 (org-ref-version)
+
+;; Koma-script report
+(add-to-list 'org-latex-classes
+          '("scrreprt" "\\documentclass[11pt]{scrreprt}"
+            ("\\part{%s}" . "\\part*{%s}")
+            ("\\chapter{%s}" . "\\chapter*{%s}")
+            ("\\section{%s}" . "\\section*{%s}")
+            ("\\subsection{%s}" . "\\subsection*{%s}")
+            ("\\subsubsection{%s}" . "\\subsubsection*{%s}")))
+
+;; Koma-script article
+(add-to-list 'org-latex-classes
+             '("scrartcl" "\\documentclass[11pt]{scrartcl}"
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+               ("\\paragraph{%s}" . "\\paragraph*{%s}")
+               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
